182a67e6476e2a81f51afcd20c4ead6f0ce65811
ptking777 <ptking777@example.com>
2021-02-23 09:02:28 -0500

    final GA

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_F2S1CquxU', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'v3q18cfgmfsps6on8nmu1sbpf', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://osk0cjz12h.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
